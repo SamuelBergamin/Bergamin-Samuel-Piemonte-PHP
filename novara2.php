a9034c724115c02a798eeb6b46fec1939716a535
SamuelBergamin <bergamin.samuel@gmail.com>
2017-06-13 21:06:20 +0200

    Create novara2.php

--- novara2.php
+++ novara2.php
@@ -1,16 +1,16 @@
 	<?php
-		$title = "Home";
+		$title = "centro-storico";
 		include_once 'header.php';
 	?>
 	<body>
 		<section>
 			<h1>Centro storico</h1>
-			<p>Il monumento più celebre di Novara è la <strong>Basilica di San Gaudenzio</strong>, costruita tra fine Cinquecento ed
+			<p>Il monumento piÃ¹ celebre di Novara Ã¨ la <strong>Basilica di San Gaudenzio</strong>, costruita tra fine Cinquecento ed
 			inizio Seicento, e caratterizzata dall'imponente cupola neoclassica a pinnacolo progettata da <em>Alessandro
-			Antonelli</em> e aggiunta al corpo della chiesa nella seconda metà XIX secolo, da molti considerata la più alta al
+			Antonelli</em> e aggiunta al corpo della chiesa nella seconda metÃ  XIX secolo, da molti considerata la piÃ¹ alta al
 			mondo in mattoni.</p>
-			<p>Centro della vita religiosa cittadina è l'imponente <strong>Duomo</strong>, in stile neoclassico, progettato anch'esso da
-			Alessandro Antonelli e costruito a metà del XIX secolo sui resti di una preesistente cattedrale romanica
+			<p>Centro della vita religiosa cittadina Ã¨ l'imponente <strong>Duomo</strong>, in stile neoclassico, progettato anch'esso da
+			Alessandro Antonelli e costruito a metÃ  del XIX secolo sui resti di una preesistente cattedrale romanica
 			(sorge infatti nello stesso punto in cui si trovava anticamente il tempio di Giove), della quale appartengono
 			ancora la parte inferiore del campanile, il <strong>Chiostro della Canonica e l'Oratorio di San Siro.</strong></p>
 			<img src="images/duomo.jpg">
@@ -20,6 +20,5 @@
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 	<?php
-		$title = "Home";
 		include_once 'footer.php';
 	?>
