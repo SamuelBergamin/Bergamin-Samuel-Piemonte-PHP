178559587cb24ea367bb1c71fa7454192b609dca
SamuelBergamin <bergamin.samuel@gmail.com>
2017-06-13 21:40:10 +0200

    Create novara1.php

--- novara1.php
+++ novara1.php
@@ -1,7 +1,13 @@
-<?php
+	<?php
 		$title = "monumenti-e-luoghi-di-interesse";
 		include_once 'header.php';
 	?>
+		<div id="container" class="container-fluid">
+				<div class="row">
+				<div class="col-md-3">
+				</div>
+					
+		<div class="col-md-8">	
 		<section>
 			<h1>Monumenti e luoghi d'interesse</h1>
 			<p>L'antico nucleo cittadino di Novara, di forma pressoché pentagonale, è situato su un modesto rilievo
@@ -11,6 +17,7 @@
 			<img src="images/mura-romane.jpg">
 			<p>Resti delle mura romane</p>
 		</section>
+		</div>
 		<!-- /.container -->
 		<!-- Bootstrap core JavaScript
 		================================================== -->
