8c8d226df6433d0fad15ee273bfa7431a45e991b
SamuelBergamin <bergamin.samuel@gmail.com>
2017-06-13 21:07:49 +0200

    Create novara3.php

--- novara3.php
+++ novara3.php
@@ -1,5 +1,5 @@
 	<?php
-		$title = "Home";
+		$title = "architetture-civili";
 		include_once 'header.php';
 	?>
 	<body>
@@ -8,16 +8,16 @@
 			<ul>
 				<li><strong>Arengo del Broletto</strong> (XII-XV secolo).
 				</li>
-				<li><strong>Palazzo Tornielli-Bellini</strong> (inizio XVI secolo), dal 1905 Ë la sede centrale della <em>Banca Popolare di
+				<li><strong>Palazzo Tornielli-Bellini</strong> (inizio XVI secolo), dal 1905 √® la sede centrale della <em>Banca Popolare di
 				Novara</em>.
 				</li>
-				<li><strong>Ospedale Maggiore della Carit‡</strong>, edificato a partire dal 1628 ed ampliato nei secoli successivi ad
-				opera di vari architetti tra i quali Alessandro Antonelli, a met‡ dell'Ottocento.
+				<li><strong>Ospedale Maggiore della Carit√†</strong>, edificato a partire dal 1628 ed ampliato nei secoli successivi ad
+				opera di vari architetti tra i quali Alessandro Antonelli, a met√† dell'Ottocento.
 				</li>
 				<li><strong>Teatro Coccia</strong> (fine del XIX secolo), inaugurato il 22 dicembre 1888. L'attuale edificio sorge sulle
 				spoglie del settecentesco teatro <em>Morelli</em> (poi dedicato a Carlo Coccia), abbattuto per permettere la
 				nuova costruzione, opera dell'architetto Giuseppe Oliverio. Oltre a personaggi illustri come <em>Arturo</em>
-				Toscanini e il compianto <em>Guido Cantelli</em>, in anni pi˘ recenti ha ospitato il programma televisivo
+				Toscanini e il compianto <em>Guido Cantelli</em>, in anni pi√π recenti ha ospitato il programma televisivo
 				<em>Bravo Bravissimo</em>, condotto da Mike Bongiorno.
 				</li>
 				<li><strong>Casa Quaroni</strong> (XX secolo), testimonianza di stile liberty a Novara.
@@ -32,7 +32,6 @@
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 	<?php
-		$title = "Home";
 		include_once 'footer.php';
 	?>
 		
