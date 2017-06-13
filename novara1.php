24ddcdd2c91aac860ccafe2e72dd90bafaf70818
SamuelBergamin <bergamin.samuel@gmail.com>
2017-06-13 21:05:30 +0200

    Create novara1.php

--- novara1.php
+++ novara1.php
@@ -1,11 +1,11 @@
 <?php
-		$title = "Home";
+		$title = "monumenti-e-luoghi-di-interesse";
 		include_once 'header.php';
 	?>
 	<body>
 		<section>
 			<h1>Monumenti e luoghi d'interesse</h1>
-			<p>L'antico nucleo cittadino di Novara, di forma pressoché pentagonale, è situato su un modesto rilievo
+			<p>L'antico nucleo cittadino di Novara, di forma pressochÃ© pentagonale, Ã¨ situato su un modesto rilievo
 			collinare (oggi Centro Storico, sede dell'omonima circoscrizione) e conserva ancora per gran parte,
 			nonostante le pesanti manomissioni in chiave moderna e le molte architetture neoclassiche, l'antico impianto
 			medievale con strade ciottolate e piccole piazze (<em>piazza delle Erbe e piazza della Repubblica</em>).</p>
@@ -16,7 +16,6 @@
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 	<?php
-		$title = "Home";
 		include_once 'footer.php';
 	?>
 		
