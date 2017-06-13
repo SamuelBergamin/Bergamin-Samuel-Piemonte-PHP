34402060edcad6debee0571c17bfecb956305b03
SamuelBergamin <bergamin.samuel@gmail.com>
2017-06-13 20:49:43 +0200

    Create alpi.php

--- alpi.php
+++ alpi.php
@@ -1,5 +1,5 @@
 	<?php
-		$title = "Home";
+		$title = "alpi";
 		include_once 'header.php';
 	?>
 			<div id="container" class="container-fluid">
@@ -19,14 +19,14 @@
 					
 			
 			
-			<p>Le <strong>Alpi</strong> sono la catena montuosa più importante d’Europa. Si chiamano in francese <em>Alpes</em>, in tedesco <em>Alpen</em>,
+			<p>Le <strong>Alpi</strong> sono la catena montuosa piÃ¹ importante dâ€™Europa. Si chiamano in francese <em>Alpes</em>, in tedesco <em>Alpen</em>,
 			in sloveno <em>Alpe</em>. Sesto Pompeo Festo nel suo <em>Primo Libro</em> attesta che il nome deriva da <em>ALBUS</em> (bianco) che
 			i Sabini pronunciavano <em>Alpus</em> e indicava il colore sempre bianco della catena innevata anche durante la
 			stagione estiva.<p>
-			<p>Per convenzione le Alpi iniziano a ovest del Colle di Cadibona, anche se in realtà il confine geologico è
-			posto nei pressi di Genova ed è costituito dal Gruppo di Voltri, terminano a ovest della città di <a href="http://www.vienna.it">Vienna</a>,
-			coprendo una distanza di circa 1.300 km a forma di arco tra l’Italia Settentrionale, la Francia sud-orientale,
-			la Svizzera, il Liechtenstein, l’Austria, il sud della Germania, la Slovenia e l’Ungheria occidentale. Tra
+			<p>Per convenzione le Alpi iniziano a ovest del Colle di Cadibona, anche se in realtÃ  il confine geologico Ã¨
+			posto nei pressi di Genova ed Ã¨ costituito dal Gruppo di Voltri, terminano a ovest della cittÃ  di <a href="http://www.vienna.it">Vienna</a>,
+			coprendo una distanza di circa 1.300 km a forma di arco tra lâ€™Italia Settentrionale, la Francia sud-orientale,
+			la Svizzera, il Liechtenstein, lâ€™Austria, il sud della Germania, la Slovenia e lâ€™Ungheria occidentale. Tra
 			Verona e Monaco di Baviera raggiungono la larghezza massima.</p>
 			<img src="images/alpi.jpg ">
 			<p><a href="index.php">Torna alla pagina principale</a></p>
@@ -39,6 +39,5 @@
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 	<?php
-		$title = "Home";
 		include_once 'footer.php';
 	?>
